d5281ada19415c96724c0574d772989215355f66
JHannappel <juergen@juergen-hannappel.de>
2011-07-05 12:13:45 +0000

    add photomultiplier class to inventory

--- variables.php
+++ variables.php
@@ -3,7 +3,7 @@ include 'access_data.php';
 $dbstring=$pwdconfiguration['web_inventory_dbstring'];
 
 $location_types=array('City','Building','Room','Rack','Crate','Slot','Cabinet','Module','Box');
-$model_types=array('NIM Crate','NIM Module','VME Crate','VME Module','CAMAC Crate','CAMAC Module','Rack','HV Crate','HV Module','Misc Crate','Fantray','Power Supply','Board','Computer','Monitor','DIN-Rail stuff','Detector','Various');
+$model_types=array('NIM Crate','NIM Module','VME Crate','VME Module','CAMAC Crate','CAMAC Module','Rack','HV Crate','HV Module','Misc Crate','Fantray','Power Supply','Board','Computer','Monitor','DIN-Rail stuff','Detector','Photomultiplier','Various');
 $maintenance_states=array('Working','Broken','Problems','Notice');
 
 ?>
