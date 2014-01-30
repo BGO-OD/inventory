63efe5f3af47bf673dab3966a8e70bad4aba5893
heinervdm <bugs@vdm-design.de>
2014-01-30 11:18:51 +0000

    orders.php: add company column and add selection inks to companies and orderers

--- orders.php
+++ orders.php
@@ -7,6 +7,12 @@
 		return DateTime::createFromFormat('Y-m-d G:i:s', $date)->format('d.m.Y');
 	}
 
+	if (isset($_GET['condition'])) {
+		$condition=" WHERE ".$_GET['condition'];
+	} else {
+		$condition="";
+	}
+
 	page_head("Orders","B1 inventory: Orders");
 	$dbconn = pg_connect($dbstring);
 	if (!$dbconn) {
@@ -20,6 +26,7 @@
 	echo "<tr class=\"rundbhead\">";
 	echo "<td>Order Number</td>";
 	echo "<td>Summary</td>";
+	echo "<td>Company</td>";
 	echo "<td>Order Date</td>";
 	echo "<td>Invoice Date</td>";
 	echo "<td>Price netto/brutto/paid (Currency)</td>";
@@ -28,15 +35,16 @@
 	echo "<td>Signature</td>";
 	echo "</tr>\n";
 	
-	$result = pg_query($dbconn, "SELECT number, name, orderdate, invoicedate, netto, brutto, currency, amount, besteller, (select count(*) from ordercomments where ordercomments.number = orders.number) as comments,(SELECT name FROM users WHERE userid=signed_by) AS signature, (SELECT count(id) FROM objects WHERE order_number LIKE ('%05/' || substring(concat('0',number) from 3) || '%')) AS inventorycounts, (SELECT name FROM users WHERE userid=besteller) as bestellername FROM orders ORDER BY number DESC;");
+	$result = pg_query($dbconn, "SELECT number, name, company, orderdate, invoicedate, netto, brutto, currency, amount, besteller, (select count(*) from ordercomments where ordercomments.number = orders.number) as comments,(SELECT name FROM users WHERE userid=signed_by) AS signature, (SELECT count(id) FROM objects WHERE order_number LIKE ('%05/' || substring(concat('0',number) from 3) || '%')) AS inventorycounts, (SELECT name FROM users WHERE userid=besteller) as bestellername FROM orders ${condition} ORDER BY number DESC;");
 	while ($row=pg_fetch_assoc($result)) {
 		echo "<tr class=\"rundbrun\">";
 		echo "<td>"; if ($row['inventorycounts'] > 0) echo "<a href=\"objects.php?condition=order_number LIKE '%2505/".substr($row['number'],1)."%25'\">05/".substr($row['number'],1)."</a>"; else echo "05/".substr($row['number'],1); echo "</td>";
-		echo "<td><a href=\"order.php?order=${row['number']}\">{$row['name']}</a></td>";
+		echo "<td><a href=\"order.php?order={$row['number']}\">{$row['name']}</a></td>";
+		echo "<td><a href=\"orders.php?condition=company='{$row['company']}'\">{$row['company']}</a></td>";
 		echo "<td>"; if (!empty($row['orderdate'])) echo dateFromPostgres($row['orderdate']);   echo "</td>";
 		echo "<td>"; if (!empty($row['invoicedate'])) echo dateFromPostgres($row['invoicedate']); echo "</td>";
 		echo "<td>{$row['netto']}/{$row['brutto']}/{$row['amount']} ({$row['currency']})</td>";
-		echo "<td>{$row['bestellername']}</td>";
+		echo "<td><a href=\"orders.php?condition=besteller={$row['besteller']}\">{$row['bestellername']}</a></td>";
 		echo "<td>{$row['comments']}</td>";
 		echo "<td>{$row['signature']}</td>";
 		echo "</tr>\n";
