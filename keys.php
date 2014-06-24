0d187a9f52fd40683a598e3440f14230f440d914
heinervdm <bugs@vdm-design.de>
2014-06-24 10:57:58 +0000

    inventory key list: link keys to objects

--- keys.php
+++ keys.php
@@ -24,12 +24,24 @@ echo "<td>With Hall Access</td>";
 echo "<td>No Hall Access</td>";
 echo "</tr>\n";
 
-$result = pg_query($dbconn, "SELECT serial, (SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user, (SELECT serial FROM objects where location=p.id and comment='hall access') as withhallaccess, (SELECT serial FROM objects where location=p.id and comment='without hall access') as withouthallaccess FROM objects as p WHERE model=299 ORDER BY serial::int;");
+$result = pg_query($dbconn, "
+	SELECT
+		id,
+		serial,
+		(SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user,
+		(SELECT serial FROM objects where location=p.id and comment='hall access') as withhallaccess,
+		(SELECT id FROM objects where location=p.id and comment='hall access') as withhallaccessid,
+		(SELECT serial FROM objects where location=p.id and comment='without hall access') as withouthallaccess,
+		(SELECT id FROM objects where location=p.id and comment='without hall access') as withouthallaccessid
+	FROM objects as p
+	WHERE model=299
+	ORDER BY serial::int;
+	");
 while ($row=pg_fetch_assoc($result)) {
 	echo "\t\t<tr class=\"rundbrun\">";
-	echo "<td>{$row['serial']}</td>";
-	echo "<td>{$row['withhallaccess']}</td>";
-	echo "<td>{$row['withouthallaccess']}</td>";
+	echo "<td><a href=\"object.php?object={$row['id']}\">{$row['serial']}</a></td>";
+	echo "<td><a href=\"object.php?object={$row['withhallaccessid']}\">{$row['withhallaccess']}</a></td>";
+	echo "<td><a href=\"object.php?object={$row['withouthallaccessid']}\">{$row['withouthallaccess']}</a></td>";
 	echo "<td>{$row['user']}</td>";
 	echo "</tr>\n";
 }
@@ -41,12 +53,24 @@ echo "<td>No Hall Access</td>";
 echo "<td>Used by:</td>";
 echo "</tr>\n";
 
-$result = pg_query($dbconn, "SELECT serial, (SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user, (SELECT serial FROM objects where location=p.id and comment='hall access') as withhallaccess, (SELECT serial FROM objects where location=p.id and comment='without hall access') as withouthallaccess FROM objects as p WHERE model=300 ORDER BY serial::int;");
+$result = pg_query($dbconn, "
+	SELECT
+		id,
+		serial,
+		(SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user,
+		(SELECT serial FROM objects where location=p.id and comment='hall access') as withhallaccess,
+		(SELECT id FROM objects where location=p.id and comment='hall access') as withhallaccessid,
+		(SELECT serial FROM objects where location=p.id and comment='without hall access') as withouthallaccess,
+		(SELECT id FROM objects where location=p.id and comment='without hall access') as withouthallaccessid
+	FROM objects as p
+	WHERE model=300
+	ORDER BY serial::int;
+	");
 while ($row=pg_fetch_assoc($result)) {
 	echo "\t\t<tr class=\"rundbrun\">";
-	echo "<td>{$row['serial']}</td>";
-	echo "<td>{$row['withhallaccess']}</td>";
-	echo "<td>{$row['withouthallaccess']}</td>";
+	echo "<td><a href=\"object.php?object={$row['id']}\">{$row['serial']}</a></td>";
+	echo "<td><a href=\"object.php?object={$row['withhallaccessid']}\">{$row['withhallaccess']}</a></td>";
+	echo "<td><a href=\"object.php?object={$row['withouthallaccessid']}\">{$row['withouthallaccess']}</a></td>";
 	echo "<td>{$row['user']}</td>";
 	echo "</tr>\n";
 }
@@ -58,12 +82,24 @@ echo "<td>No Hall Access</td>";
 echo "<td>Used by:</td>";
 echo "</tr>\n";
 
-$result = pg_query($dbconn, "SELECT serial, (SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user, comment FROM objects as p WHERE (SELECT model FROM objects WHERE id=p.location) != 300 AND (SELECT model FROM objects WHERE id=p.location) != 299 AND model=301 ORDER BY serial;");
+$result = pg_query($dbconn, "
+	SELECT
+		id,
+		serial,
+		(SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user,
+		comment
+	FROM objects as p
+	WHERE
+		(SELECT model FROM objects WHERE id=p.location) != 300 AND
+		(SELECT model FROM objects WHERE id=p.location) != 299 AND
+		model=301
+	ORDER BY serial;
+	");
 while ($row=pg_fetch_assoc($result)) {
 	echo "\t\t<tr class=\"rundbrun\">";
 	echo "<td></td>";
-	if ($row['comment'] == "hall access") echo "<td>{$row['serial']}</td>"; else echo "<td></td>";
-	if ($row['comment'] == "without hall access") echo "<td>{$row['serial']}</td>"; else echo "<td></td>";
+	if ($row['comment'] == "hall access") echo "<td><a href=\"object.php?object={$row['id']}\">{$row['serial']}</a></td>"; else echo "<td></td>";
+	if ($row['comment'] == "without hall access") echo "<td><a href=\"object.php?object={$row['id']}\">{$row['serial']}</a></td>"; else echo "<td></td>";
 	echo "<td>{$row['user']}</td>";
 	echo "</tr>\n";
 }
@@ -73,10 +109,17 @@ echo "<td colspan=\"3\">Wesselhalle (1/4)</td>";
 echo "<td>Used by:</td>";
 echo "</tr>\n";
 
-$result = pg_query($dbconn, "SELECT (SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user, serial FROM objects as p WHERE model=302 ORDER BY serial::int;");
+$result = pg_query($dbconn, "
+	SELECT 
+		(SELECT users.name FROM users, usage WHERE users.userid=usage.userid AND usage.id=p.id AND usage.validfrom<now() AND usage.validto>now()) as user,
+		serial,
+		id
+	FROM objects as p
+	WHERE model=302
+	ORDER BY serial::int;");
 while ($row=pg_fetch_assoc($result)) {
 	echo "\t\t<tr class=\"rundbrun\">";
-	echo "<td colspan=\"3\">{$row['serial']}</td>";
+	echo "<td colspan=\"3\"><a href=\"object.php?object={$row['id']}\">{$row['serial']}</a></td>";
 	echo "<td>{$row['user']}</td>";
 	echo "</tr>\n";
 }
