c2a2e86803e0fc9eac2920d414b0e3872d2ed0d0
hammannd <github@dhammann.de>
2016-01-15 14:23:07 +0000

    barcode.php no longer needed

--- barcode.php
+++
@@ -1,5 +0,0 @@
-<?php
-$code=trim($_GET['number'],"'");
-header("Content-Type: image/png");
-passthru("/bin/bash ./create_label.sh $code | convert -density 150  - -background white -flatten png:-");
-?>
