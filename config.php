46011f6c4faddc9a390297cafd907054e917c24a
Oliver Freyermuth <o.freyermuth@googlemail.com>
2016-09-08 09:28:42 +0200

    Convert access-data magic to just be part of config.php.

--- config.php
+++ config.php
@@ -1,6 +1,20 @@
 <?php
-	$project_name="My Inventory";
-	if (file_exists("config.local.php")) {
-	        include("config.local.php");
-	}
+$project_name="My Inventory";
+
+$DB_HOST="localhost";
+$DB_PORT="5432";
+$DB_NAME="inventory";
+$DB_USER="inventory";
+$DB_PASSWORD="myPassword";
+
+$maintenance_states=array('Working','Broken','Problems','Notice');
+
+if (file_exists("config.local.php")) {
+	  include("config.local.php");
+}
+
+if (!isset($dbstring)) {
+		$dbstring="host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD";
+}
+
 ?>
--- website/variables.php
+++ website/variables.php
@@ -1,10 +1,3 @@
 <?php
-include '../common/access_data.php';
-
 include '../config.php';
-
-$dbstring=$pwdconfiguration['web_inventory_dbstring'];
-
-$maintenance_states=array('Working','Broken','Problems','Notice');
-
 ?>
